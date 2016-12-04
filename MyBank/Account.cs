614e2c731c292bdf08d772e97f9cbba88215bcc9
shuai <shuai@DESKTOP-EFUB88H>
2016-12-04 09:53:20 -0800

    Initial commit

---
+++ MyBank/Account.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+
+    public enum TypeOfAccounts
+    {
+        Checking,
+        Savings
+    }
+    /// <summary>
+    /// This class describes a bank account
+    /// 
+    /// </summary>
+    public class Account
+    {
+        #region Statics
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Account number of the account
+        /// </summary>
+        public int AccountNumber {
+            get;
+            private set; }
+        /// <summary>
+        /// Email address of the user
+        /// </summary>
+        public string EmailAddress{ get; set; }
+        public decimal Balance {
+            get;
+            private set; }
+        public TypeOfAccounts AccountType { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Account()
+        {
+            //lastAccountNumber++;
+            //AccountNumber = lastAccountNumber;
+
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        /// <returns>The new balance</returns>
+        public decimal Deposit (decimal amount)
+        {
+            //Balance = Balance + amount;
+            Balance += amount;
+            return Balance;
+        }
+
+
+        public decimal Withdraw(decimal amount)
+        {
+            if (amount <=Balance)
+            {
+                Balance -= amount;
+            }
+            return Balance;
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************");
+            Console.WriteLine("Wlcome to my bank!");
+            var choice="-1";
+            while (true)
+            {
+                Console.WriteLine("Please select from the list of choises");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Creat Account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("Have a great day!");
+                        return;
+                    case "1":
+                        Console.Write("Email Address: ");
+                        var emailAddress = Console.ReadLine();
+                        var checkingAccount = new Account();
+                        checkingAccount.EmailAddress = emailAddress;
+                        checkingAccount.AccountType = TypeOfAccounts.Checking; checkingAccount.Deposit(1002.50M);
+                        Console.WriteLine($"Account Number: {checkingAccount.AccountNumber}, Balance: {checkingAccount.Balance:C}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+    }
+}
